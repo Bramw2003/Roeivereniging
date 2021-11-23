a84a97e27a5d94d1bade36e56a3ba99392982ca1
Bram de Smidt <bwdesmidt@gmail.com>
2021-11-23 20:50:58 +0100

    Added MVVM bases

---
+++ Roeivereniging/Viewmodel/BoatViewmodel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using MicroMvvm;
+using Model;
+namespace Viewmodel
+{
+    public class BoatViewmodel : ObservableObject
+    {
+        private Boat _boat;
+        public BoatViewmodel()
+        {
+            _boat = new Boat(0,"Unknown",1,1,false,true);
+        }
+
+        public Boat Boat
+        {
+            get { return _boat; }
+            set { _boat = value; }
+        }
+
+        public string BoatName
+        {
+            get { return _boat.name; }
+        }
+    }
+}
