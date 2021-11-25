20514ed101a999a234756cae7bbb7cfc93d4bf75
Bram de Smidt <bwdesmidt@gmail.com>
2021-11-25 21:09:52 +0100

    Added reservation class

---
+++ Roeivereniging/Model/Reservation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model
+{
+    public class Reservation
+    {
+        public DateTime startTime { get; }
+        public DateTime endTime { get; }
+        public Model.Boat boat { get; }
+        public Model.Member member { get; }
+
+        public Reservation(DateTime startTime, DateTime endTime, int boatId, int MemberId)
+        {
+            this.startTime = startTime;
+            this.endTime = endTime;
+            this.boat = Model.DAO.Boat.GetBoatByID(boatId);
+            this.member = Model.DAO.Member.GetById(MemberId);
+        }
+    }
+}
