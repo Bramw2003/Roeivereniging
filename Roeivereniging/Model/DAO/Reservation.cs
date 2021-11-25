1611b2b369f55a206c99acee7ce7558342ae67c9
Bram de Smidt <bwdesmidt@gmail.com>
2021-11-25 22:17:58 +0100

    Updated DAO for reservations

--- Roeivereniging/Model/DAO/Reservation.cs
+++ Roeivereniging/Model/DAO/Reservation.cs
@@ -5,8 +5,57 @@ using System.Text;
 
 namespace Model.DAO
 {
-    internal class Reservation
+    public static class Reservation
     {
+        public static List<Model.Reservation> GetAll()
+        {
+            Database.Init();
+            String sql = "SELECT starttime, boatID, endtime, LIDID, [date] FROM reservations";
+            List<Model.Reservation> list = new List<Model.Reservation>();
+            SqlCommand command = new SqlCommand(sql, Database.connection);
+            if (Database.OpenConnection())
+            {
+                var a = command.ExecuteReader();
+                while (a.Read())
+                {
+                    DateTime start = a.GetDateTime(0);
+                    DateTime end = a.GetDateTime(2);
+                    DateTime date = a.GetDateTime(4).Date;
+                    start = date + new TimeSpan(start.Hour, start.Minute, start.Second);
+                    end = date + new TimeSpan(end.Hour, end.Minute, end.Second);
+                    Model.Reservation n = new Model.Reservation(start, end, a.GetInt32(4), a.GetInt32(1));
+                    list.Add(n);
+                }
+                command.Dispose();
+                Database.connection.Close();
+            }
+            return list;
+        }
+        public static List<Model.Reservation> GetAllByMember(Model.Member member)
+        {
+            Database.Init();
+            String sql = "SELECT starttime, boatID, endtime, LIDID, [date] FROM reservations WHERE LIDID=@id";
+            List<Model.Reservation> list = new List<Model.Reservation>();
+            SqlCommand command = new SqlCommand(sql, Database.connection);
+            command.Parameters.AddWithValue("id",member.GetId());
+            if (Database.OpenConnection())
+            {
+                var a = command.ExecuteReader();
+                while (a.Read())
+                {
+                    DateTime start = a.GetDateTime(4).Date;
+                    DateTime end = a.GetDateTime(4).Date;
+
+                    start = start + a.GetTimeSpan(0);
+                    end = end + a.GetTimeSpan(2);
+                    Model.Reservation n = new Model.Reservation(start, end, a.GetInt32(1), a.GetInt32(3));
+                    list.Add(n);
+                }
+                command.Dispose();
+                Database.connection.Close();
+            }
+            return list;
+        }
         public static bool ReserveBoat(Model.Boat boat, DateTime date, DateTime endTime, Model.Member member)
         {
             Database.Init();
@@ -14,11 +63,11 @@ namespace Model.DAO
 
             using (SqlCommand command = new SqlCommand(sql, Database.connection))
             {
-                command.Parameters.AddWithValue("starttime",date.ToString("HH:mm:ss"));
+                command.Parameters.AddWithValue("starttime", date.ToString("HH:mm:ss"));
                 command.Parameters.AddWithValue("boatid", boat.id);
                 command.Parameters.AddWithValue("endtime", endTime.ToString("HH:mm:ss"));
                 command.Parameters.AddWithValue("memberid", member.GetId());
-                command.Parameters.AddWithValue("date",date.Date);
+                command.Parameters.AddWithValue("date", date.Date);
                 bool result = (int)command.ExecuteScalar() == 1;
                 command.Dispose();
                 Database.connection.Close();
