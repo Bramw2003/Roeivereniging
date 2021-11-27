d29e830a1b278725c4f31fe0f28fa074382dc226
Bram de Smidt <bwdesmidt@gmail.com>
2021-11-27 17:18:50 +0100

    Added filtering to the UI

--- Roeivereniging/View/ReservePage.xaml.cs
+++ Roeivereniging/View/ReservePage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -22,23 +23,24 @@ namespace View
         public ReservePage()
         {
             InitializeComponent();
-            EndTime.IsEnabled = false;
-            Date.IsEnabled = false;
+            StartTime.Minimum = DateTime.Now;
+            EndTime.Minimum = DateTime.Now;
+            Date.DisplayDateStart = DateTime.Now;
+            Date.DisplayDateEnd = DateTime.Now.AddDays(5);
         }
 
         private void StartTime_ValueChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
         {
-            EndTime.IsEnabled = true;
             EndTime.Minimum = (DateTime)e.NewValue;
             EndTime.Maximum = ((DateTime)e.NewValue).AddHours(6);
             EndTime.ClipValueToMinMax = true;
+            UpdateDuration();
             UpdateLbBoats();
         }
 
         private void EndTime_ValueChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
         {
-            Date.IsEnabled = true;
-            Date.DisplayDateEnd = DateTime.Now.AddDays(5);
+            UpdateDuration();
             UpdateLbBoats();
         }
 
@@ -49,10 +51,20 @@ namespace View
         }
         public void UpdateLbBoats()
         {
-            if (Date.SelectedDate != null && StartTime.Value != null && EndTime.Value != null)
+            if (Date?.SelectedDate != null && StartTime.Value != null && EndTime.Value != null && TbPersons.Text != "")
             {
                 var date = (DateTime)Date.SelectedDate;
-                LbBoats.ItemsSource = Database.GetAvailableBoats(date, (DateTime)StartTime.Value, (DateTime)EndTime.Value);
+                LbBoats.ItemsSource = Database.GetAvailableBoats(date, (DateTime)StartTime.Value, (DateTime)EndTime.Value,(BoatType)CbType.SelectedIndex,int.Parse(TbPersons.Text),(bool)ChbSteer.IsChecked, (bool)ChbScull.IsChecked);
+            }
+        }
+
+        private void UpdateDuration()
+        {
+            if (StartTime.Value != null && EndTime.Value != null)
+            {
+                DateTime startTime = (DateTime)StartTime.Value.Value;
+                DateTime endTime = (DateTime)EndTime.Value.Value;
+                LbDuur.Content = "Duur: " + (endTime - startTime).Hours + ":" + (endTime - startTime).Minutes;
             }
         }
 
@@ -69,6 +81,35 @@ namespace View
                 DateTime end = (DateTime)Date.SelectedDate.Value.Date + b;
                 MessageBox.Show(Model.DAO.Reservation.ReserveBoat((Boat)LbBoats.SelectedItem, date, end, MainWindow.currentMember).ToString());
             }
+        }
+
+
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        private void CbType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateLbBoats();
+        }
+
+        private void TbPersons_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateLbBoats();
+        }
+
+        private void ChbSteer_Click(object sender, RoutedEventArgs e)
+        {
+            UpdateLbBoats();
+
+        }
+
+        private void ChbScull_Click(object sender, RoutedEventArgs e)
+        {
+            UpdateLbBoats();
+
         }
     }
 }
