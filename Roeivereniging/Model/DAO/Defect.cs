525544c78be621c698ffc4c65bc7499bafd24689
Bram de Smidt <bwdesmidt@gmail.com>
2021-11-29 12:53:43 +0100

    Added Defect class and DAO class

---
+++ Roeivereniging/Model/DAO/Defect.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace Model.DAO
+{
+    public static class Defect
+    {
+        public static List<Model.Defect> GetAll()
+        {
+            Database.Init();
+
+            string sql = "SELECT [title], [description], [LIDID], [boatID] FROM [brokenboat]";
+            List<Model.Defect> list = new List<Model.Defect>();
+            SqlCommand command = new SqlCommand(sql, Database.connection);
+            if (Database.OpenConnection())
+            {
+                var a = command.ExecuteReader();
+                while (a.Read())
+                {
+                    Model.Defect defect = new Model.Defect(a.GetString(0),a.GetString(1),Member.GetById(a.GetInt32(2)),Boat.GetBoatByID(a.GetInt32(3)));
+                    list.Add(defect);
+                }
+                command.Dispose();
+                Database.connection.Close();
+            }
+            return list;
+        }
+        public static List<Model.Defect> GetByBoatID(int boatID)
+        {
+            Database.Init();
+
+            string sql = "SELECT [title], [description], [LIDID], [boatID] FROM [brokenboat] WHERE [boatID]=@id";
+            List<Model.Defect> list = new List<Model.Defect>();
+            SqlCommand command = new SqlCommand(sql, Database.connection);
+            command.Parameters.AddWithValue("id", boatID);
+            if (Database.OpenConnection())
+            {
+                var a = command.ExecuteReader();
+                while (a.Read())
+                {
+                    Model.Defect defect = new Model.Defect(a.GetString(0), a.GetString(1), Member.GetById(a.GetInt32(2)), Boat.GetBoatByID(a.GetInt32(3)));
+                    list.Add(defect);
+                }
+                command.Dispose();
+                Database.connection.Close();
+            }
+            return list;
+        }
+        public static List<Model.Defect> GetByBoat(Model.Boat boat)
+        {
+            return GetByBoatID(boat.id);
+        }
+        public static bool Add(Model.Defect defect)
+        {
+            Database.Init();
+            string sql = "INSERT INTO brokenboat(title, description, LIDID, boatID) VALUES (@title, @desc, @memberID, @boatID)";
+            SqlCommand command = new SqlCommand(sql, Database.connection);
+            command.Parameters.AddWithValue("title", defect.title);
+            command.Parameters.AddWithValue("desc", defect.description);
+            command.Parameters.AddWithValue("memberID", defect.reporter.GetId());
+            command.Parameters.AddWithValue("boatID", defect.boat.id);
+            if (Database.OpenConnection())
+            {
+                return command.ExecuteNonQuery() == 1;
+            }
+            command.Dispose();
+            Database.connection.Dispose();
+            return false;
+        }
+    }
+}
---
+++ Roeivereniging/Model/Defect.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model
+{
+    public class Defect
+    {
+        public string title { get; set; }
+        public string description { get; set; }
+        public Member reporter { get; set; }
+        public Boat boat { get; set; }
+
+        public Defect(string title, string description, Member reporter, Boat boat)
+        {
+            this.title = title;
+            this.description = description;
+            this.reporter = reporter;
+            this.boat = boat;
+        }
+    }
+}
