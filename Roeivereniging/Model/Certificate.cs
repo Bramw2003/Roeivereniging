249153fe6afd329ffc5c60e2859f392c553d683e
Bram de Smidt <bwdesmidt@gmail.com>
2021-12-08 20:41:39 +0100

    Added public variables

---
+++ Roeivereniging/Model/Certificate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model
+{
+    public class Certificate
+    {
+        public DateTime date { get; set; }
+        public string name { get; }
+        public Certificate(string name, DateTime? date = null)
+        {
+            this.name = name;
+            if(date == null)
+            {
+                date = DateTime.Now;
+            }
+            this.date = date.Value;
+        }
+    }
+}
--- Roeivereniging/Model/Member.cs
+++ Roeivereniging/Model/Member.cs
@@ -5,95 +5,47 @@ namespace Model
 {
     public class Member
     {
-        private int _id { get; }
-        private DateTime _date { get; set; }
-        private string _name { get; set; }
-        private bool _admin { get; set; }
-        private bool _repair { get; set; }
-        private bool _examinator { get; set; }
-        private string _username { get; set; }
-        private string _email { get; set; }
+        public int id { get; }
+        public DateTime date { get; }
+        public string name { get; }
+        public bool admin { get; }
+        public bool repair { get; }
+        public bool examinator { get; }
+        public string username { get; }
+        public string email { get; }
 
         public Member(int id, string name, string username, DateTime date, bool admin, bool repair, bool examinator, string email)
         {
-            _id = id;
-            _name = name;
-            _username = username;
-            _date = date.Date;
-            _admin = admin;
-            _repair = repair;
-            _examinator = examinator;
-            _email = email;
+            this.id = id;
+            this.name = name;
+            this.username = username;
+            this.date = date.Date;
+            this.admin = admin;
+            this.repair = repair;
+            this.examinator = examinator;
+            this.email = email;
         }
 
-        public Member(string name, string username, DateTime date, string email) {
-            _name = name;
-            _username = username;
-            _date = date;
-            _email = email;
+        public Member(string name, string username, DateTime date, string email)
+        {
+            this.name = name;
+            this.username = username;
+            this.date = date;
+            this.email = email;
         }
 
         #region Getters
-        public DateTime GetBirthday() { return _date; }
-        public string GetName() { return _name; }
-        public bool IsAdmin() { return _admin; }
-        public bool IsRepair() { return _repair; }
-        public bool IsExaminator() { return _examinator; }
-        public string GetUsername() { return _username; }
-        public int GetId() { return _id; }
-        public string GetEmail() { return _email; }
+        public DateTime GetBirthday() { return date; }
+        public string GetName() { return name; }
+        public bool IsAdmin() { return admin; }
+        public bool IsRepair() { return repair; }
+        public bool IsExaminator() { return examinator; }
+        public string GetUsername() { return username; }
+        public int GetId() { return id; }
+        public string GetEmail() { return email; }
         #endregion
 
-        #region Setters
-        public bool SetBirthday(DateTime date) { return Alter(date: date); }
-        public bool SetName(string name) { return Alter(name: name); }
-        public bool SetUsername(string username) { return Alter(username: username); }
-        public bool SetAdmin(bool admin) { return Alter(admin: admin); }
-        public bool SetRepair(bool repair) { return Alter(repair: repair); }
-        public bool SetExaminator (bool examinator) { return Alter(examinator: examinator); }
-        #endregion
 
-        private bool Alter(DateTime? date = null, bool? admin = null, bool? repair = null, bool? examinator = null, string username = null, string name = null)
-        {
 
-            date = date ?? _date;
-            admin = admin ?? _admin;
-            repair = repair ?? _repair;
-            examinator = examinator ?? _examinator;
-            username = username ?? _username;
-            name = name ?? _name;
-
-            Database.Init();
-            string sql = "UPDATE [member] SET [name] = @name, [birthday] = @bday, [admin] = @admin, [repair] = @repair, [examinator] = @examinator, [username] = @username WHERE [ID] = @id";
-            bool result = false;
-            SqlCommand command = new SqlCommand(sql, Database.connection);
-            command.Parameters.AddWithValue("username", username);
-            command.Parameters.AddWithValue("name", name);
-            command.Parameters.AddWithValue("bday", date.Value.Date);
-            command.Parameters.AddWithValue("admin", admin.Value);
-            command.Parameters.AddWithValue("repair", repair.Value);
-            command.Parameters.AddWithValue("examinator", examinator.Value);
-            command.Parameters.AddWithValue("id", _id);
-            if (Database.OpenConnection())
-            {
-                var a = command.ExecuteScalar();
-                if (a != null)
-                {
-                    result = (int)a == 1;
-                }
-                command.Dispose();
-                Database.connection.Close();
-            }
-            if (result)
-            {
-                _date = date.Value;
-                _admin = admin.Value;
-                _repair = repair.Value;
-                _examinator = examinator.Value;
-                _username = username;
-                _name = name;
-            }
-            return result;
-        }
     }
 }
