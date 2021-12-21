7197eefe185c657660a5889dbd440dc2ad23870e
PatrickSlag <patrickslag@gmail.com>
2021-12-21 16:15:57 +0100

    added editboatwindow

---
+++ Roeivereniging/View/Admin/EditBoatWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace View.Admin
+{
+    /// <summary>
+    /// Interaction logic for EditBoatWindow.xaml
+    /// </summary>
+    public partial class EditBoatWindow : Window
+    {
+        public EditBoatWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
